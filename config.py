d56247b60ca1de021aaa704acf79637cb8388081
Sahil Sharma <s.sharma@planradar.com>
2025-05-14 14:55:02 +0200

    Fix PGSQL URL

--- config.py
+++ config.py
@@ -3,12 +3,19 @@ import os
 class Config:
     PROJECT_NAME = os.getenv("PROJECT_NAME", "Flask CRUD App")
 
-    # Ensure these are explicitly set in the runtime environment
     JWT_SECRET = os.environ["JWT_SECRET"]
     JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
-    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+    DB_USER = os.environ["DB_USER"]
+    DB_PASSWORD = os.environ["DB_PASSWORD"]
+    DB_HOST = os.environ["DB_HOST"]
+    DB_PORT = os.environ["DB_PORT"]
+    DB_NAME = os.environ["DB_NAME"]
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     OTLP_ENDPOINT = os.environ["OTLP_ENDPOINT"]
